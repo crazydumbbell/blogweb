d205be0b8c06cfd5b48cc1bf715cd4926e2d6e19
Radish007 <58253441+crazydumbbell@users.noreply.github.com>
2024-03-04 14:48:05 +0900

    blogweb

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  WebViewController webViewController = WebViewController()
+    ..loadRequest(Uri.parse('https://blog.codefactory.ai'))
+    ..setJavaScriptMode(JavaScriptMode.unrestricted);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: const Text('Code Factory'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              webViewController.loadRequest(
+                Uri.parse('https://blog.codefactory.ai'),
+              );
+            },
+            icon: const Icon(
+              Icons.home,
+            ),
+          ),
+        ],
+      ),
+      body: WebViewWidget(controller: webViewController),
+    );
+  }
+}
